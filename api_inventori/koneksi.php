078b0396a28dd25ed17120e0acac57070b96bac4
adamyhy <tamat4tahunamin@gmail.com>
2020-08-07 21:47:48 +0700

    free database server for testing

--- api_inventori/koneksi.php
+++ api_inventori/koneksi.php
@@ -1,10 +1,10 @@
 
 <?php
 
-$db_host	= "localhost";
-$db_user	= "root";
-$db_pass	= "";
-$db_name	= "db_inventori_uir";
+$db_host	= "remotemysql.com";
+$db_user	= "MvqFYgKMvs";
+$db_pass	= "FTv75OHHPC";
+$db_name	= "MvqFYgKMvs";
 
 $conn = mysqli_connect($db_host,$db_user,$db_pass,$db_name) or die ("Gagal Koneksi Ke Server");
 ?>
